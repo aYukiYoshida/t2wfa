4a71a2a2c83861a5e036f36917480ab0d874288e
aYukiYoshida <yuki.yoshida@access-company.com>
2024-12-12 23:26:20 +0900

    style: change color of input key component

--- src/components/ui/input-key.tsx
+++ src/components/ui/input-key.tsx
@@ -23,14 +23,14 @@ const InputKey: FC<InputKeyProps> = ({setShowImageCard}): JSX.Element => {
     <div className="flex items-center space-x-4">
       <Input
         type="text"
-        className="w-96 mt-4 px-4 py-2"
+        className="w-96 mt-4 px-4 py-2 text-white border-gray-400 border-2 placeholder-red placeholder-opacity-0"
         value={inputKeyValue}
         onChange={handleInputKeyChange}
         placeholder="Enter API KEY of APOD"
       />
       <Button
         onClick={handleSaveKeyClick}
-        className="mt-4 px-4 py-2 text-white rounded-md hover:bg-gray-700"
+        className="mt-4 px-4 py-2 bg-gray-400 text-white background rounded-md hover:bg-gray-700"
       >
         Save
       </Button>
