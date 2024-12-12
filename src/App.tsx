b75999b0ce426c5e9f49704a6ab49230e685224d
aYukiYoshida <yuki.yoshida@access-company.com>
2024-12-12 22:15:40 +0900

    feat: add input key component

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,19 @@
 import {useState} from "react";
 import {ImageCard} from "./components/ui/image-card";
-import {Button} from "@/components/ui/button";
-import {Input} from "@/components/ui/input";
-import setCookie from "@/utils/cookie/setCookie";
+import {InputKey} from "./components/ui/input-key";
 import getCookie from "@/utils/cookie/getCookie";
 
 function App() {
-  const [inputKeyValue, setInputKeyValue] = useState<string>("");
   const [showImageCard, setShowImageCard] = useState<boolean>(false);
 
-  const handleInputKeyChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setInputKeyValue(event.target.value);
-  };
-
-  const handleSaveKeyClick = () => {
-    setCookie("key", inputKeyValue);
-    setShowImageCard(true);
-  };
-
   return (
-    <>
-      {!showImageCard && (
-        <div className="flex items-center justify-center min-h-screen">
-          <div className="flex items-center space-x-4">
-            <Input
-              type="text"
-              className="w-96 mt-4 px-4 py-2"
-              value={inputKeyValue}
-              onChange={handleInputKeyChange}
-              placeholder="Enter API KEY of APOD"
-            />
-            <Button
-              onClick={handleSaveKeyClick}
-              className="mt-4 px-4 py-2 text-white rounded-md hover:bg-gray-700"
-            >
-              Save
-            </Button>
-          </div>
-        </div>
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-blue-900 to-gray-900">
+      {showImageCard ? (
+        <ImageCard apiKey={getCookie("key")} />
+      ) : (
+        <InputKey setShowImageCard={setShowImageCard} />
       )}
-      {showImageCard && <ImageCard apiKey={getCookie("key")}></ImageCard>}
-      {}
-    </>
+    </div>
   );
 }
 
---
+++ src/components/ui/input-key.tsx
@@ -0,0 +1,41 @@
+import {useState, FC, Dispatch} from "react";
+import {Button} from "@/components/ui/button";
+import {Input} from "@/components/ui/input";
+import setCookie from "@/utils/cookie/setCookie";
+
+interface InputKeyProps {
+  setShowImageCard: Dispatch<boolean>;
+}
+
+const InputKey: FC<InputKeyProps> = ({setShowImageCard}) => {
+  const [inputKeyValue, setInputKeyValue] = useState<string>("");
+
+  const handleInputKeyChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputKeyValue(event.target.value);
+  };
+
+  const handleSaveKeyClick = () => {
+    setCookie("key", inputKeyValue);
+    setShowImageCard(true);
+  };
+
+  return (
+    <div className="flex items-center space-x-4">
+      <Input
+        type="text"
+        className="w-96 mt-4 px-4 py-2"
+        value={inputKeyValue}
+        onChange={handleInputKeyChange}
+        placeholder="Enter API KEY of APOD"
+      />
+      <Button
+        onClick={handleSaveKeyClick}
+        className="mt-4 px-4 py-2 text-white rounded-md hover:bg-gray-700"
+      >
+        Save
+      </Button>
+    </div>
+  );
+};
+
+export {InputKey};
