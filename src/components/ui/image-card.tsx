3aa9b38d159555c30ba5c3cac01c69ebc6765435
aYukiYoshida <yuki.yoshida@access-company.com>
2025-05-27 15:19:22 +0900

    refactor: remove unnecessary class from CardTitle

--- src/components/ui/image-card.tsx
+++ src/components/ui/image-card.tsx
@@ -20,7 +20,7 @@ const ImageCard: FC = (): JSX.Element => {
   return (
     <Card className="w-full max-w-md bg-gradient-to-br from-gray-900 to-gray-800 text-white shadow-xl overflow-scroll my-10">
       <CardHeader>
-        <CardTitle className="text-3xl text-white text-center">
+        <CardTitle className="text-3xl text-center">
           Astronomy Picture of the Day
         </CardTitle>
         <CardDescription className="text-white text-center">
