01fce801add1b5a819b866affd4943e7aa604f64
aYukiYoshida <yuki.yoshida@access-company.com>
2025-05-27 16:05:11 +0900

    feat: change layout of message in image card

--- src/components/ui/image-card.tsx
+++ src/components/ui/image-card.tsx
@@ -1,11 +1,5 @@
 import {FC} from "react";
-import {
-  Card,
-  CardHeader,
-  CardTitle,
-  CardContent,
-  CardDescription,
-} from "@/components/ui/card";
+import {Card, CardHeader, CardTitle, CardContent} from "@/components/ui/card";
 import {ExpandableText} from "@/components/ui/expandable-text";
 import {DatePickerDialog} from "@/components/ui/date-picker-dialog";
 import Hooks from "@/lib/hooks";
@@ -13,34 +7,35 @@ import Hooks from "@/lib/hooks";
 const ImageCard: FC = (): JSX.Element => {
   const {image, loading, error} = Hooks.useFetchApodImage();
 
-  if (loading) return <p className="text-white">Loading...</p>;
-  if (!image || error)
-    return <p className="text-white">Error: {error?.message}</p>;
-
   return (
     <Card className="w-full max-w-md bg-gradient-to-br from-gray-900 to-gray-800 text-white shadow-xl overflow-scroll my-10">
       <CardHeader>
         <CardTitle className="text-3xl text-center">
           Astronomy Picture of the Day
         </CardTitle>
-        <CardDescription className="text-white text-center">
-          {image.date}
-        </CardDescription>
         <DatePickerDialog />
       </CardHeader>
       <CardContent>
-        <div className="relative aspect-square mb-6 overflow-hidden rounded-lg shadow-2xl">
-          <img
-            src={image.url}
-            alt="Cover"
-            className="w-full h-full object-cover transition-transform duration-500 ease-out hover:scale-110"
-          />
-        </div>
-        <div className="text-center mb-2">
-          <h2 className="text-2xl font-bold m-1">{image.title}</h2>
-          <p className="text-gray-400">&copy; {image.copyright}</p>
-        </div>
-        <ExpandableText text={image.explanation}></ExpandableText>
+        {loading ? (
+          <p className="text-white text-center">Loading...</p>
+        ) : !image || error ? (
+          <p className="text-white text-center">Error: {error?.message}</p>
+        ) : (
+          <>
+            <div className="relative aspect-square mb-6 overflow-hidden rounded-lg shadow-2xl">
+              <img
+                src={image.url}
+                alt="Cover"
+                className="w-full h-full object-cover transition-transform duration-500 ease-out hover:scale-110"
+              />
+            </div>
+            <div className="text-center mb-2">
+              <h2 className="text-2xl font-bold m-1">{image.title}</h2>
+              <p className="text-gray-400">&copy; {image.copyright}</p>
+            </div>
+            <ExpandableText text={image.explanation}></ExpandableText>
+          </>
+        )}
       </CardContent>
     </Card>
   );
