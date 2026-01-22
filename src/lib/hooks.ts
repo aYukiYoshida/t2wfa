76dc515b5890df3a3551b79bf1910e25ebb845fd
jyoppomu <jyoppomu@gmail.com>
2026-01-22 13:36:13 +0900

    feat: reflect invalid API key state if 403 error occurs

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -1,12 +1,14 @@
 import {useState, useEffect} from "react";
 
 import Api from "@/lib/api";
+import {InvalidApiKeyError} from "@/lib/errors";
 import {useAuthStore, useDateStore} from "@/lib/store";
 import {ApodImageResponse} from "@/lib/types";
 
 const useFetchApodImage = () => {
   const key: string = useAuthStore((state) => state.token) ?? "DEMO_KEY";
   const date: Date | undefined = useDateStore((state) => state.date);
+  const setApiKeyValid = useAuthStore((state) => state.setTokenValid);
   const [image, setImage] = useState<ApodImageResponse>();
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<Error | null>(null);
@@ -22,6 +24,10 @@ const useFetchApodImage = () => {
         setImage(imageResponse);
         setError(null);
       } catch (err) {
+        if (err instanceof InvalidApiKeyError) {
+          // 403エラー時は無効キーとしてフラグを更新
+          setApiKeyValid(false);
+        }
         setError(err as Error);
       } finally {
         setLoading(false);
@@ -29,7 +35,7 @@ const useFetchApodImage = () => {
     };
 
     fetchImage();
-  }, [key, date]);
+  }, [key, date, setApiKeyValid]);
 
   return {image, loading, error};
 };
