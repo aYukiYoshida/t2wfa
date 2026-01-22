facbece02e8f51e2a5f9b8caee24c591c194b0fa
jyoppomu <jyoppomu@gmail.com>
2026-01-22 11:45:58 +0900

    refactor: simplify ImageCard and ImageContent

--- src/components/ui/image-card.tsx
+++ src/components/ui/image-card.tsx
@@ -1,14 +1,10 @@
 import {FC} from "react";
 
-import {Card, CardHeader, CardTitle, CardContent} from "@/components/ui/card";
+import {Card, CardHeader, CardTitle} from "@/components/ui/card";
 import {DatePickerDialog} from "@/components/ui/date-picker-dialog";
 import {ImageContent} from "@/components/ui/image-content";
-import {MessageText} from "@/components/ui/message-text";
-import Hooks from "@/lib/hooks";
 
 const ImageCard: FC = (): JSX.Element => {
-  const {image, loading, error} = Hooks.useFetchApodImage();
-
   return (
     <Card className="w-full max-w-md bg-gradient-to-br from-gray-900 to-gray-800 text-white shadow-xl overflow-scroll my-10">
       <CardHeader>
@@ -17,15 +13,7 @@ const ImageCard: FC = (): JSX.Element => {
         </CardTitle>
         <DatePickerDialog />
       </CardHeader>
-      <CardContent>
-        {loading ? (
-          <MessageText text="Loading..." />
-        ) : !image || error ? (
-          <MessageText text={`Error: ${error?.message}`} />
-        ) : (
-          <ImageContent image={image} />
-        )}
-      </CardContent>
+      <ImageContent />
     </Card>
   );
 };
--- src/components/ui/image-content.tsx
+++ src/components/ui/image-content.tsx
@@ -1,29 +1,36 @@
 import {FC} from "react";
 
+import {CardContent} from "@/components/ui/card";
 import {ExpandableText} from "@/components/ui/expandable-text";
+import {MessageText} from "@/components/ui/message-text";
+import Hooks from "@/lib/hooks";
 
-import type {ApodImageResponse} from "@/lib/types";
+const ImageContent: FC = (): JSX.Element => {
+  const {image, loading, error} = Hooks.useFetchApodImage();
 
-type ImageContentProps = {
-  image: ApodImageResponse;
-};
-
-const ImageContent: FC<ImageContentProps> = ({image}): JSX.Element => {
   return (
-    <>
-      <div className="relative aspect-square mb-6 overflow-hidden rounded-lg shadow-2xl">
-        <img
-          src={image.url}
-          alt="Cover"
-          className="w-full h-full object-cover transition-transform duration-500 ease-out hover:scale-110"
-        />
-      </div>
-      <div className="text-center mb-2">
-        <h2 className="text-2xl font-bold m-1">{image.title}</h2>
-        <p className="text-gray-400">&copy; {image.copyright}</p>
-      </div>
-      <ExpandableText text={image.explanation}></ExpandableText>
-    </>
+    <CardContent>
+      {loading ? (
+        <MessageText text="Loading..." />
+      ) : !image || error ? (
+        <MessageText text={`Error: ${error?.message}`} />
+      ) : (
+        <>
+          <div className="relative aspect-square mb-6 overflow-hidden rounded-lg shadow-2xl">
+            <img
+              src={image.url}
+              alt="Cover"
+              className="w-full h-full object-cover transition-transform duration-500 ease-out hover:scale-110"
+            />
+          </div>
+          <div className="text-center mb-2">
+            <h2 className="text-2xl font-bold m-1">{image.title}</h2>
+            <p className="text-gray-400">&copy; {image.copyright}</p>
+          </div>
+          <ExpandableText text={image.explanation}></ExpandableText>
+        </>
+      )}
+    </CardContent>
   );
 };
 
