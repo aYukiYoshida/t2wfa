9ca6793d645851a132c0a60de95acced3cb2e592
jyoppomu <jyoppomu@gmail.com>
2026-01-22 14:19:55 +0900

    feat: redesign App component to integrate InputKey and ImageContent

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import {Background} from "@/components/ui/background";
-import {ImageCard} from "@/components/ui/image-card";
+import {Card, CardHeader, CardTitle} from "@/components/ui/card";
+import {DatePickerDialog} from "@/components/ui/date-picker-dialog";
+import {ImageContent} from "@/components/ui/image-content";
 import {InputKey} from "@/components/ui/input-key";
 import {useAuthStore} from "@/lib/store";
 
@@ -8,19 +10,23 @@ function App() {
   const isApiKeyValid = useAuthStore((state) => state.isApiKeyValid);
 
   // APIキーがあっても無効な場合は再入力画面を表示
-  if (apiKey !== "" && isApiKeyValid) {
-    return (
-      <Background className="items-start">
-        <ImageCard />
-      </Background>
-    );
-  } else {
-    return (
-      <Background className="items-center">
-        <InputKey showInvalidMessage={!isApiKeyValid} />
-      </Background>
-    );
-  }
+  return (
+    <Background className="items-start">
+      <Card className="w-full max-w-md bg-gradient-to-br from-gray-900 to-gray-800 text-white shadow-xl overflow-scroll my-10">
+        <CardHeader>
+          <CardTitle className="text-3xl text-center">
+            Astronomy Picture of the Day
+          </CardTitle>
+          <DatePickerDialog />
+        </CardHeader>
+        {apiKey !== "" && isApiKeyValid ? (
+          <ImageContent />
+        ) : (
+          <InputKey showInvalidMessage={!isApiKeyValid} />
+        )}
+      </Card>
+    </Background>
+  );
 }
 
 export default App;
--- src/components/ui/input-key.tsx
+++ src/components/ui/input-key.tsx
@@ -1,6 +1,7 @@
 import {useRef, FC} from "react";
 
 import {Button} from "@/components/ui/button";
+import {CardContent} from "@/components/ui/card";
 import {Input} from "@/components/ui/input";
 import {useAuthStore} from "@/lib/store";
 
@@ -21,26 +22,27 @@ const InputKey: FC<InputKeyProps> = ({
   };
 
   return (
-    <div className="text-white">
-      <h1 className="text-3xl pb-6">Astronomy Picture of the Day</h1>
-      <div className="flex items-center space-x-4">
-        <Input
-          type="text"
-          className="w-96 border-gray-400 border-2 placeholder-red placeholder-opacity-0"
-          ref={inputApiKey}
-          placeholder="Enter API KEY of APOD"
-        />
-        <Button
-          onClick={handleSaveKeyClick}
-          className="bg-gray-400 background rounded-md hover:bg-gray-700"
-        >
-          Save
-        </Button>
+    <CardContent>
+      <div className="text-white">
+        <div className="flex items-center space-x-4">
+          <Input
+            type="text"
+            className="w-96 border-gray-400 border-2 placeholder-red placeholder-opacity-0"
+            ref={inputApiKey}
+            placeholder="Enter API KEY of APOD"
+          />
+          <Button
+            onClick={handleSaveKeyClick}
+            className="bg-gray-400 background rounded-md hover:bg-gray-700"
+          >
+            Save
+          </Button>
+        </div>
+        {showInvalidMessage && (
+          <p className="text-red-500 pt-1">Input API key is invalid.</p>
+        )}
       </div>
-      {showInvalidMessage && (
-        <p className="text-red-500 pt-1">Input API key is invalid.</p>
-      )}
-    </div>
+    </CardContent>
   );
 };
 
